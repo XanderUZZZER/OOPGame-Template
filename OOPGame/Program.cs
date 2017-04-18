19431771defae37dcf6852c90ebffa7d19e02e03
Oleksandr Lukhanin <xander_uzzzer@mail.ru>
2017-04-18 16:52:27 +0300

    Add project files.

---
+++ OOPGame/Program.cs
@@ -0,0 +1,23 @@
+﻿using NConsoleGraphics;
+using System;
+
+namespace OOPGame {
+
+  public class Program {
+
+    static void Main(string[] args) {
+
+      Console.WindowWidth = 40;     //width in columns
+      Console.WindowHeight = 40;    //height in rows
+      Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+      Console.BackgroundColor = ConsoleColor.White;
+      Console.CursorVisible = false;
+      Console.Clear();
+
+      ConsoleGraphics graphics = new ConsoleGraphics();
+
+      GameEngine engine = new SampleGameEngine(graphics);
+      engine.Start();
+    }
+  }
+}
